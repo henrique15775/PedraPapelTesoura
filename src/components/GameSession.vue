f067c3d6470bde4c724e05728274868a5a568f50
henrique15775 <lhdeveloper137@gmail.com>
2022-08-02 11:54:38 -0300

    commit5

--- src/components/GameSession.vue
+++ src/components/GameSession.vue
@@ -1,14 +1,22 @@
 <template>
     <div id="gameSession">
-        <div>
-            Rock
-            Paper
-            Scissors
+        <div id="bar-game">
+            <div id="idonou">
+                <p>Rock</p>
+                <p>Paper</p>
+                <p>Scissors</p>
+            </div>
+            <div id="score-bar">
+                <h4 id="score-title">Score</h4>
+                <span id="score">14</span>
+            </div>
         </div>
     </div>
 </template>
 
 <script>
+    
+    import '../assets/css/components/GameSession.css'
 
     export default {
     name: "GameSession",
